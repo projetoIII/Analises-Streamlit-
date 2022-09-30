97c1979f7d9cc0390825606cc1ff1a19614e8b59
Thays Silva <thaysconceicaods@gmail.com>
2022-09-30 11:00:51 -0300

    questao 3 - estrutura

--- main.py
+++ main.py
@@ -182,7 +182,23 @@ def questao2():
         st.write("Não houveram investimentos para o órgão selecionado no período.")
 
 def questao3():
-    st.subheader('**3 - Gasto total nos 3 primeiros meses do ano e por estado**')
+    st.subheader('**3 - Soma de gastos pagos, liquidados e empenhados por programa do governo e por estado a em determinado mês**')
+
+    st.sidebar.markdown('## Programa de governo')
+    programas = run_query("SELECT DISTINCT nome_programa_governo, programa_governo_id FROM programas_governo")
+    programas_base = []
+    programas_id = []
+
+    for programa in programas:
+        og = str(programa)
+        if og[2:-5] != "NAO ATRIBUIDO" and og[2:-5] != "Indefinido" and og[2:-5] != "Sem informação":
+            programas_base.append(og[2:-5])
+            programas_id.append(og[-3:-1])
+
+    programa = st.sidebar.selectbox('Selecione o programa de governo', options=programas_base, key=1)
+
+    programa_index = programas_base.index(programa)
+    programa_id = programas_id[programa_index]
 
     st.sidebar.markdown('## Estado')
     estados_base = run_query("SELECT DISTINCT uf, local_id FROM localidade")
@@ -196,32 +212,44 @@ def questao3():
             estados.append(uf[2:-5])
             estados_id.append(uf[-3:-1])
 
-    estado = st.sidebar.selectbox('Selecione o estado que deseja consultar os gastos semanais', options=estados)
+    estado = st.sidebar.selectbox('Selecione o estado que deseja consultar os gastos semanais', options=estados, key=2)
 
     estado_index = estados.index(estado)
     estado_id = estados_id[estado_index]
 
-    st.sidebar.markdown('## Tempo')
-    tempo_base = run_query("SELECT DISTINCT data_lancamento, tempo_id FROM tempo where tempo_id != 0")
-    data_lancamento = []
-    tempo_id = []
+    st.sidebar.markdown('## Ano')
+    ano_base = run_query("SELECT DISTINCT ano, tempo_id FROM tempo")
+    anos = []
 
-    for tempo in tempo_base:
-        tp = str(tempo)
-        data_lancamento.append(tp[2:-5])
-        tempo_id.append(tp[-3:-1])
+    for ano in ano_base:
+        tp = str(ano)
+        if (tp[2:-5] != "0000") and (tp[2:-5] not in anos):
+            tp = str(ano)
+            anos.append(tp[2:-5])
 
-    tempo = st.sidebar.selectbox('Selecione a data de lançamento que deseja consultar', options=data_lancamento)
+    ano = st.sidebar.selectbox('Selecione o ano que deseja saber a porcentagem', options=anos, key=3)
 
-    data_index = data_lancamento.index(tempo)
-    data_id = tempo_id[data_index]
+    st.sidebar.markdown('## Mês')
+    meses_base = run_query("SELECT DISTINCT mes, tempo_id FROM tempo")
+    meses = []
+    meses_id = []
 
-    gastos_base = run_query("select sum(valor_pago) as valor_total "
-                            "FROM fato_despesas f INNER JOIN tempo t ON t.tempo_id = f.tempo_id "
-                            "WHERE f.localidade_id = {0} and f.tempo_id = {1} GROUP BY data_lancamento".format(estado_id, data_id))
+    for mes in meses_base:
+        ms = str(mes)
+        if ms[2:-5] != "Não especificada":
+            ms = str(mes)
+            meses.append(ms[2:-5])
+            meses_id.append(ms[-3:-1])
 
-    print(gastos_base)
-    st.write('R$',str(gastos_base[0][0]))
+    mes = st.sidebar.selectbox('Selecione o mês que deseja saber a porcentagem', options=meses, key=4)
+    mes_index = meses.index(mes)
+    mes_id = meses_id[mes_index]
+
+    #alterar query
+    #gastos_base = run_query()
+
+    #alterar ainda o modo de exibicao
+    #st.write('R$',str(gastos_base[0][0]))
 
 
 def questao4():
